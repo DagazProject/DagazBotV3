089951b67f770ce110a3b58328e0d2b6078d2a71
DagazProject <dagaz-project@mail.ru>
2024-07-22 16:13:18 +0300

    upload image (fixed)

--- src/data-source.ts
+++ src/data-source.ts
@@ -34,6 +34,7 @@ import { session_type } from "./entity/session_type"
 import { session } from "./entity/session"
 import { session_param } from "./entity/session_param"
 import { user_session } from "./entity/user_session"
+import { image } from "./entity/image"
 
 export const db = new DataSource({
   type: "postgres",
@@ -44,7 +45,7 @@ export const db = new DataSource({
   database: "dagaz-bot",
   synchronize: true,
   logging: false,
-  entities: [global_param, global_value, global_fixup, global_log, users, service, user_service, script, command, user_context, param_type, param_value, message, client_message, action_type, server, action, localized_string, request_param, response_param, account, task, command_param, macro, macro_param, delta_type, text_type, quest_text, info, user_info, session_type, session, session_param, user_session],
+  entities: [global_param, global_value, global_fixup, global_log, users, service, user_service, script, command, user_context, param_type, param_value, message, client_message, action_type, server, action, localized_string, request_param, response_param, account, task, command_param, macro, macro_param, delta_type, text_type, quest_text, info, user_info, session_type, session, session_param, user_session, image],
   subscribers: [],
   migrations: []
 })
