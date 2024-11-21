3025f4ca238f2e9a5a180795476400ea9b370439
DagazProject <dagaz-project@mail.ru>
2024-11-21 11:47:53 +0300

    GLAVRED (fixed)

--- src/utils.ts
+++ src/utils.ts
@@ -1374,7 +1374,7 @@ export async function execWrite(bot, chatId, service, id) {
                 qm.params[i] = p;
             }*/
 
-/*          for (let i = 0; i < qm.jumps.length; i++) {
+            for (let i = 0; i < qm.jumps.length; i++) {
                 for (let j = 0; j < qm.jumps[i].paramsConditions.length; j++) {
                     if (qm.jumps[i].paramsConditions[j].mustEqualValues.length == 0 && qm.jumps[i].paramsConditions[j].mustEqualValuesEqual) {
                         qm.jumps[i].paramsConditions[j].mustEqualValuesEqual = false;
@@ -1383,7 +1383,7 @@ export async function execWrite(bot, chatId, service, id) {
                         qm.jumps[i].paramsConditions[j].mustModValuesMod = false;
                     }
                 }
-            }*/
+            }
 
             const buf = writeQmm(qm);
             fs.writeFileSync(__dirname + '/../upload/quest.qmm', buf);
